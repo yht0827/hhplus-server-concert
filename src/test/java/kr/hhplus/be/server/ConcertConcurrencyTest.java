48fc612eea5123393c100df8ef383894365b029a
yht0827 <yangheetae0827@naver.com>
2025-01-18 10:36:31 +0900

    chore: 포인트 충전 동시성 테스트

--- src/test/java/kr/hhplus/be/server/ConcertConcurrencyTest.java
+++ src/test/java/kr/hhplus/be/server/ConcertConcurrencyTest.java
@@ -15,9 +15,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import kr.hhplus.be.server.application.PointFacade;
 import kr.hhplus.be.server.application.ReservationFacade;
+import kr.hhplus.be.server.domain.point.entity.Point;
+import kr.hhplus.be.server.domain.point.repository.PointRepository;
 import kr.hhplus.be.server.domain.reservation.entity.Reservation;
+import kr.hhplus.be.server.infrastructure.concert.ConcertJpaRepository;
+import kr.hhplus.be.server.infrastructure.point.PointHistoryJpaRepository;
+import kr.hhplus.be.server.infrastructure.point.PointJpaRepository;
 import kr.hhplus.be.server.infrastructure.reservation.ReservationJpaRepository;
+import kr.hhplus.be.server.interfaces.point.dto.ChargeRequest;
 import kr.hhplus.be.server.interfaces.reservation.dto.ReserveRequest;
 import lombok.extern.slf4j.Slf4j;
 
@@ -33,9 +40,68 @@ public class ConcertConcurrencyTest {
 	@Autowired
 	private ReservationFacade reservationFacade;
 
+	@Autowired
+	private ConcertJpaRepository concertJpaRepository;
+
+	@Autowired
+	private PointFacade pointFacade;
+
+	@Autowired
+	private PointJpaRepository pointJpaRepository;
+
+	@Autowired
+	private PointRepository pointRepository;
+
+	@Autowired
+	private PointHistoryJpaRepository pointHistoryJpaRepository;
+
 	@BeforeEach
 	void tearDown() {
 		reservationJpaRepository.deleteAllInBatch();
+		pointJpaRepository.deleteAllInBatch();
+		pointHistoryJpaRepository.deleteAllInBatch();
+	}
+
+	@DisplayName("동시에 포인트 충전")
+	@Test
+	void testConcurrencyChargePoint() throws InterruptedException {
+		int numberOfThreads = 10;
+
+		Point point = Point.builder()
+			.userId(1L)
+			.balance(0)
+			.build();
+
+		pointJpaRepository.save(point);
+
+		ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+		CountDownLatch latch = new CountDownLatch(numberOfThreads);
+
+		ChargeRequest chargeRequest = ChargeRequest.builder()
+			.point(5000)
+			.userId(1L)
+			.build();
+
+		for (int i = 0; i < numberOfThreads; i++) {
+
+			executorService.submit(() -> {
+				try {
+					pointFacade.chargePoint(chargeRequest);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+		executorService.shutdown();
+
+		int balance = pointJpaRepository.findAll()
+			.stream()
+			.mapToInt(Point::getBalance)
+			.sum();
+
+		assertEquals(50000, balance);
 	}
 
 	@DisplayName("동시에 여러 요청이 와도 콘서트 하나만 예약된다.")
@@ -49,7 +115,7 @@ public class ConcertConcurrencyTest {
 		for (int i = 0; i < numberOfThreads; i++) {
 			ReserveRequest reserveRequest = ReserveRequest.builder()
 				.userId((long)i + 1)
-				.concertId(2L)
+				.concertId(1L)
 				.seatNumber(1)
 				.build();
 
