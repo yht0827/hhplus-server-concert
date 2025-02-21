0c8cc095d6b012097f36f0bf622abdd8830bd623
yht0827 <yangheetae0827@naver.com>
2025-02-21 09:14:28 +0900

    :test_tube: 카프카 연결 테스트

--- src/test/java/kr/hhplus/be/server/support/TestContainerConfiguration.java
+++ src/test/java/kr/hhplus/be/server/support/TestContainerConfiguration.java
@@ -5,7 +5,10 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
+
+import jakarta.annotation.PreDestroy;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestContainerConfiguration {
@@ -22,5 +25,23 @@ public class TestContainerConfiguration {
 	GenericContainer<?> redisContainer() {
 		return new GenericContainer<>(DockerImageName.parse("redis:7.4.2")).withExposedPorts(6379)
 			.withReuse(true);
+	}
+
+	public static final ConfluentKafkaContainer KAFKA_CONTAINER;
+
+	static {
+		KAFKA_CONTAINER = new ConfluentKafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.8.1"));
+		KAFKA_CONTAINER.start();
+
+		System.setProperty("spring.kafka.bootstrap-servers", KAFKA_CONTAINER.getBootstrapServers());
+		System.setProperty("spring.kafka.consumer.bootstrap-servers", KAFKA_CONTAINER.getBootstrapServers());
+		System.setProperty("spring.kafka.producer.bootstrap-servers", KAFKA_CONTAINER.getBootstrapServers());
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+		if (KAFKA_CONTAINER.isRunning()) {
+			KAFKA_CONTAINER.stop();
+		}
 	}
 }
